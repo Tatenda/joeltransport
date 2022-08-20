28e7b3717f1d8bf4f8997e84bf055ee71ac98c60
mathase <m.mashau95@gmail.com>
2022-08-20 17:39:47 +0200

    About update

--- page-about.php
+++ page-about.php
@@ -1,83 +1,101 @@
 <?php get_header(); ?>
- <!-- about -->
- <div class="about">
-      <div class="container">
+<!-- about -->
+<div class="about">
+    <div class="container">
         <div class="row">
-          <div class="col-md-6">
-            <h3>about</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum
-              amet delectus, iusto esse quas nulla similique beatae ipsa natus,
-              praesentium rerum doloremque suscipit. Tempore rem officiis
-              voluptatem quos eligendi vero.
-            </p>
-          </div>
-          <div class="col-md-5">
-            <div class="abour_img">
-              <img src="" alt="" />
+            <div class="col-md-6">
+                <h3>about</h3>
+                <p>
+                    Joel Transport was established in 1965 and has been a household
+                    name synonymous with excellent service in the furniture removals
+                    industry since inception. Joel Transport has looked after the
+                    relocation needs of ambassadors and diplomats, Managing directors
+                    and transferees. <br />
+                    <br />
+                    Huge office relocations that lasted weeks, distribution for some
+                    of the country’s finest institutions. We have the capacity to
+                    attend to your airfreights – import or export, Your sea shipment
+                    from South Africa To Australia or any destination in the world and
+                    vice versa ! Any local of domestic removal is a breeze. <br /><br />
+                    Joel Transport is a new age South African company that boasts with
+                    a 70% PDI management and ownership. We are based in Centurion
+                    Gauteng and service domestic relocations in the northern, central,
+                    western, eastern areas of South Africa. Imports into these areas
+                    and exports from them are our speciality
+                </p>
             </div>
-          </div>
+            <div class="col-md-5">
+                <div class="about_img">
+                    <img src="<?php bloginfo('template_directory') ?>/img/Thumbnail_danger_600px.jpg" alt="" />
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-    <!-- leadership -->
-    <div class="leadership">
-      <div class="container">
+</div>
+<!-- leadership -->
+<div class="leadership">
+    <div class="container">
         <h3>leadership</h3>
         <div class="row">
-          <div class="col-md-4">
-            <div class="leadership_img"></div>
-            <h5>Full name</h5>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum,
-              officiis corporis harum voluptatem rerum quasi delectus, sapiente
-              magnam natus repellendus porro repellat pariatur assumenda omnis
-              rem similique obcaecati minus quaerat.
-            </p>
-          </div>
-          <div class="col-md-4">
-            <div class="leadership_img"></div>
-            <h5>Full name</h5>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum,
-              officiis corporis harum voluptatem rerum quasi delectus, sapiente
-              magnam natus repellendus porro repellat pariatur assumenda omnis
-              rem similique obcaecati minus quaerat.
-            </p>
-          </div>
-          <div class="col-md-4">
-            <div class="leadership_img"></div>
-            <h5>Full name</h5>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum,
-              officiis corporis harum voluptatem rerum quasi delectus, sapiente
-              magnam natus repellendus porro repellat pariatur assumenda omnis
-              rem similique obcaecati minus quaerat.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- team -->
-    <div class="team">
-      <div class="container">
-        <div class="team_box">
-          <div class="row">
-            <div class="col-md-8 m-auto">
-              <div class="team_img"></div>
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi
-                nostrum blanditiis error molestiae obcaecati velit tempore
-                cumque. Repellendus voluptatum iure eveniet sit officiis
-                recusandae, fuga repudiandae aut eligendi obcaecati quam.
-              </p>
+            <div class="col-md-4">
+                <div class="leadership_img">
+                    <img src="<?php bloginfo('template_directory') ?>/img/sphiwe.svg" alt="" />
+                </div>
+                <h5>Sphiwe</h5>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum,
+                    officiis corporis harum voluptatem rerum quasi delectus, sapiente
+                    magnam natus repellendus porro repellat pariatur assumenda omnis
+                    rem similique obcaecati minus quaerat.
+                </p>
             </div>
-          </div>
+            <div class="col-md-4">
+                <div class="leadership_img">
+                    <img src="<?php bloginfo('template_directory') ?>/img/Peter_400px x 400px .JPG" alt="" />
+                </div>
+                <h5>Peter</h5>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum,
+                    officiis corporis harum voluptatem rerum quasi delectus, sapiente
+                    magnam natus repellendus porro repellat pariatur assumenda omnis
+                    rem similique obcaecati minus quaerat.
+                </p>
+            </div>
+            <div class="col-md-4">
+                <div class="leadership_img">
+                    <img src="<?php bloginfo('template_directory') ?>/img/JT Daniel Directors image B_400px x 400px .JPG" alt="" />
+                </div>
+                <h5>Daniel</h5>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum,
+                    officiis corporis harum voluptatem rerum quasi delectus, sapiente
+                    magnam natus repellendus porro repellat pariatur assumenda omnis
+                    rem similique obcaecati minus quaerat.
+                </p>
+            </div>
         </div>
-      </div>
     </div>
-    <!-- membership -->
-    <!-- plugin goes here -->
-    <!-- /membership -->
+</div>
+<!-- team -->
+<div class="team">
+    <div class="container">
+        <div class="team_box">
+            <div class="row">
+                <div class="col-md-8 m-auto">
+                    <div class="team_img">
+                        <img src="<?php bloginfo('template_directory') ?>/img/Team_1200px_x_600px.jpg" alt="" />
+                    </div>
+                    <p>
+                        We are committed to work environment where everyone is treated
+                        equally, where everyone is happy and enjoys his or her work.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- membership -->
+<!-- plugin goes here -->
+<!-- /membership -->
 
 <?php wp_footer(); ?>
