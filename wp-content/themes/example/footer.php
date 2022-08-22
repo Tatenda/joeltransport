049fd771b0bddecb4ae63aeddd74ffdc5ea41f58
Tawanda Abraham Makunike <tawanda@mmogomedia.com>
2022-08-22 16:43:15 +0200

    Updates

--- wp-content/themes/example/footer.php
+++
@@ -1,7 +0,0 @@
-            <footer class="footer">
-                <h1>Footer</h1>
-            </footer>
-        <?php wp_footer() ?>
-
-    </body>
-</html>
--- wp-content/themes/example/index.php
+++
@@ -1,5 +0,0 @@
-<?php get_header(); ?>
-<main>
-
-</main><!-- #site-content -->
-<?php get_footer(); ?>
