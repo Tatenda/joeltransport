d57de260ae8656f73c557d30ef6cd9e8205ac52b
Tatenda <tatenda.makunike@dotmodus.com>
2022-08-22 19:07:53 +0200

    top menu fix

--- header.php
+++ header.php
@@ -15,18 +15,36 @@
 <body>
     <header class="fixed-top">
         <div class="top-menu">
-
-            <div class="container">
-                <?php
+         <div class="container">
+                <!-- <?php
                 wp_nav_menu(array(
                     "menu" => "top-menu",
                     "container" => "",
                     "theme_location" => "top-menu",
                     'items_wrap' => '<ul id="" class="top-menu">%3$s</ul>'
                 ));
-                ?>
+                ?> -->
+
+                <!-- <ul class="top-menu">
+                    <li>+27 12 666 9055</li>
+                    <li></li>
+                    <li></li>
+                </ul> -->
+
+                <ul id="" class="top-menu row">
+                    <li  class="col-2 "><a href="tel://0731946286">+27 12 666 9055</a></li>
+                    <li class="col-3 "><a>sales@joeltransport.co.za</a></li>
+                    <li class=" col-2 offset-5">
+                        <a href="https://www.twitter.com/joeltransport"><i class="fa-brands fa-twitter"></i></a>
+                        <a href="https://www.instagram.com/joeltransport"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="https://www.facebook.com/joeltransport"><i class="fa-brands fa-facebook"></i></a>
+                    </li>
+                    <!-- <li id="menu-item-122" class="top-menu-right menu-item menu-item-type-custom menu-item-object-custom menu-item-122"><a href="https://www.instagram.com/joeltransport"><i class="fa-brands fa-instagram"></i></a></li> -->
+                    <!-- <li id="menu-item-121" class="top-menu-right menu-item menu-item-type-custom menu-item-object-custom menu-item-121"><a href="https://www.facebook.com/joeltransport"><i class="fa-brands fa-facebook"></i></a></li> -->
+                </ul>
 
             </div>
+
         </div>
         <div class="navigation">
             <div class="container">
