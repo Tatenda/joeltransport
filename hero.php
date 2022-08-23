6e50149c29b01bc32dc1e733318037f81e8355af
Tawanda Abraham Makunike <tawanda@mmogomedia.com>
2022-08-23 09:19:26 +0200

    Updates

---
+++ hero.php
@@ -0,0 +1,38 @@
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carausel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="http://joeltransport.mmogomedia.com/wp-content/uploads/2022/08/Hero-image-2_2500px-x-800px.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>We have +50 years’ moving, storage and happy customer experiences. Its truly stress-free and fun with us.</h5>
+                    <p>Enjoy the move!</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="http://joeltransport.mmogomedia.com/wp-content/uploads/2022/08/Hero-image-3_2500px-x-800px.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>We have +50 years’ moving, storage and happy customer experiences. Its truly stress-free and fun with us.</h5>
+                    <p>Enjoy the move!</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="http://joeltransport.mmogomedia.com/wp-content/uploads/2022/08/Hero_image_1_2500px_x_800px.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>We have +50 years’ moving, storage and happy customer experiences. Its truly stress-free and fun with us.</h5>
+                    <p>Enjoy the move!</p>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
